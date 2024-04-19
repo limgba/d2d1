3f8247e64a69c1b9bbdbfd4891dc759c3a765775
book <book>
2024-04-19 15:25:16 +0800

    yaml config

--- code/3rdpart/yaml/yaml.cpp
+++ code/3rdpart/yaml/yaml.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 
 std::vector<yaml*> yaml::m_yaml_vec;
-std::vector<std::string> yaml::m_key_vec;
+size_t yaml::m_last_line_tab_count = 0;
 yaml g_yaml;
 yaml::yaml()
 	: m_type(0)
@@ -77,12 +77,13 @@ int yaml::ReadFileList()
 	{
 		return ret;
 	}
+	WriteFile(&g_yaml, CONFIG_PATH_HEAD + "control_copy.yaml");
 	return 0;
 }
 
 int yaml::ReadFile(const std::string& str)
 {
-	static std::regex file_name("/(.*)\\.ya*ml");
+	static std::regex file_name(".*/(.*)\\.ya*ml");
 	std::smatch match_result;
 	if (!std::regex_search(str, match_result, file_name))
 	{
@@ -116,34 +117,6 @@ int yaml::ReadFile(const std::string& str)
 	return 0;
 }
 
-int yaml::WriteFile(yaml* _yaml, const std::string& path)
-{
-	if (nullptr == _yaml || nullptr == _yaml->value())
-	{
-		return -1;
-	}
-	std::ofstream ofs(path);
-	if (!ofs.is_open())
-	{
-		return -2;
-	}
-	int depth = 0;
-	yaml::WriteFileStr(_yaml, depth);
-
-	ofs.close();
-	return 0;
-}
-
-int yaml::WriteFileStr(yaml* _yaml, int& depth)
-{
-	if (nullptr == _yaml)
-	{
-		return -1;
-	}
-
-
-}
-
 int yaml::ReadFileName(const std::string& str)
 {
 	if (nullptr == g_yaml.value())
@@ -156,25 +129,21 @@ int yaml::ReadFileName(const std::string& str)
 	{
 		return -1;
 	}
+	yaml* current_yaml = new yaml;
 	auto file_it = file_mgr->find(str);
-	yaml* file = new yaml;
-	file->mutate_type(YAML_TYPE_ENUM_HASH);
-	file->mutate_value(new std::unordered_map<std::string, yaml*>);
 	if (file_it != file_mgr->end())
 	{
 		delete file_it->second;
-
-		file_it->second = file;
+		file_it->second = current_yaml;
 	}
 	else
 	{
-		file_mgr->emplace(str, file);
+		file_mgr->emplace(str, current_yaml);
 	}
 
 	m_yaml_vec.clear();
-	m_yaml_vec.push_back(&g_yaml);
-	m_yaml_vec.push_back(file);
-	m_key_vec.clear();
+	m_last_line_tab_count = 0;
+	m_yaml_vec.push_back(current_yaml);
 	return 0;
 }
 int yaml::ReadLine(int line, const std::string& str)
@@ -195,100 +164,194 @@ int yaml::ReadLine(int line, const std::string& str)
 		return 0;
 	}
 	const size_t tab_count = yaml::GetTabCount(str);
-	const size_t cur_size = tab_count + 2;
-	if (cur_size < m_yaml_vec.size())
+	if (!(tab_count == m_last_line_tab_count 
+		|| tab_count == m_last_line_tab_count + 1
+		|| tab_count < m_last_line_tab_count
+		))
 	{
-		m_yaml_vec.resize(cur_size);
+		return -1000000000 - line;
 	}
-	if (tab_count < m_key_vec.size())
+	yaml::CheckResize(tab_count);
+	if (m_yaml_vec.empty())
 	{
-		m_key_vec.resize(tab_count);
+		return -2000000000;
 	}
 	yaml* parent_yaml = m_yaml_vec.back();
 	if (nullptr == parent_yaml)
 	{
-		return -1;
+		return -2000000001;
 	}
+
 	static std::regex _hash("(\\w+): (.*)");
 	static std::regex _list("- (.*)");
-	yaml* current_yaml = nullptr;
-
-	size_t parent_container_size = 0;
-	if (nullptr != parent_yaml->GetHash())
-	{
-		parent_container_size = parent_yaml->GetHash()->size();
-	}
-	else if (nullptr != parent_yaml->GetVector())
-	{
-		parent_container_size = parent_yaml->GetVector()->size();
-	}
-
 	if (std::regex_match(value_str, match_result, _hash))
 	{
 		std::string key_str = match_result[1].str();
 		std::string hash_value_str = match_result[2].str();
+		if (nullptr == parent_yaml->value())
+		{
+			parent_yaml->mutate_type(YAML_TYPE_ENUM_HASH);
+			parent_yaml->mutate_value(new std::unordered_map<std::string, yaml*>);
+		}
 		if (!hash_value_str.empty())
 		{
-			ReadHash(parent_yaml->GetHash(), key_str, hash_value_str);
+			int ret = PushValue(parent_yaml, key_str, hash_value_str);
+			if (ret < 0)
+			{
+				return -2000 + ret;
+			}
 		}
 		else
 		{
-			current_yaml = new yaml;
-			current_yaml->mutate_type(YAML_TYPE_ENUM_HASH);
-			current_yaml->mutate_value(new std::unordered_map<std::string, yaml*>);
+			yaml* current_yaml = new yaml;
 			yaml::PushBack(parent_yaml, key_str, current_yaml);
 
 			m_yaml_vec.push_back(current_yaml);
-			m_key_vec.push_back(key_str);
 		}
 	}
 	else if (std::regex_match(value_str, match_result, _list))
 	{
+		if (nullptr == parent_yaml->value())
+		{
+			parent_yaml->mutate_type(YAML_TYPE_ENUM_LIST);
+			parent_yaml->mutate_value(new std::vector<yaml*>);
+		}
 
 		std::string list_value_str = match_result[1].str();
 		if (!list_value_str.empty())
 		{
-			ReadList(current_yaml->GetVector(), list_value_str);
+			std::string parent_size_str = std::to_string(parent_yaml->Size());
+			int ret = PushValue(parent_yaml, parent_size_str, list_value_str);
+			if (ret < 0)
+			{
+				return -3000 + ret;
+			}
 		}
 		else
 		{
-			current_yaml = new yaml;
-			current_yaml->mutate_type(YAML_TYPE_ENUM_LIST);
-			current_yaml->mutate_value(new std::vector<yaml*>);
-			std::string key_str = std::to_string(parent_container_size);
-			yaml::PushBack(parent_yaml, key_str, current_yaml);
+			std::string parent_size_str = std::to_string(parent_yaml->Size());
+			yaml* current_yaml = new yaml;
+			yaml::PushBack(parent_yaml, parent_size_str, current_yaml);
 
 			m_yaml_vec.push_back(current_yaml);
-			m_key_vec.push_back(key_str);
 		}
 	}
 	else
 	{
-		return -100;
+		return -10000;
 	}
 
+	m_last_line_tab_count = tab_count;
 	return 0;
+}
+
+int yaml::PushValue(yaml* parent_yaml, const std::string& key, const std::string& str)
+{
+	if (nullptr == parent_yaml)
+	{
+		return -100;
+	}
+	switch (parent_yaml->type())
+	{
+	case YAML_TYPE_ENUM_HASH:
+	{
+		auto* hash = parent_yaml->GetHash();
+		if (nullptr == hash)
+		{
+			return -200;
+		}
+		int ret = ReadHash(hash, key, str);
+		if (ret < 0)
+		{
+			return -300 + ret;
+		}
+	}
+	break;
+	case YAML_TYPE_ENUM_LIST:
+	{
+		auto* vec = parent_yaml->GetVector();
+		if (nullptr == vec)
+		{
+			return -400;
+		}
+		int ret = ReadList(vec, str);
+		if (ret < 0)
+		{
+			return -500 + ret;
+		}
+	}
+	break;
+	}
+	return 0;
+}
+
+bool yaml::PushBack(yaml* parent_yaml, const std::string& key, yaml* current_yaml)
+{
+	if (nullptr == parent_yaml || nullptr == current_yaml)
+	{
+		return false;
+	}
+
+	switch (parent_yaml->type())
+	{
+	case YAML_TYPE_ENUM_HASH:
+	{
+		std::unordered_map<std::string, yaml*>* hash = parent_yaml->GetHash();
+		if (nullptr == hash)
+		{
+			return false;
+		}
+		auto it = hash->find(key);
+		if (it != hash->end())
+		{
+			delete it->second;
+			it->second = current_yaml;
+		}
+		else
+		{
+			hash->emplace(key, current_yaml);
+		}
+	}
+	break;
+	case YAML_TYPE_ENUM_LIST:
+	{
+		std::vector<yaml*>* vec = parent_yaml->GetVector();
+		if (nullptr == vec)
+		{
+			return false;
+		}
+		vec->push_back(current_yaml);
+	}
+	break;
+	default:
+	{
+		return false;
+	}
+	break;
+	}
+
+	return true;
 }
 
 int yaml::ReadHash(std::unordered_map<std::string, yaml*>* hash, const std::string& key, const std::string& str)
 {
 	if (nullptr == hash || key.empty() || str.empty())
 	{
-		return -1;
+		return -10;
 	}
 	yaml* current_yaml = new yaml;
 	if (nullptr == current_yaml)
 	{
-		return -2;
+		return -20;
 	}
 	int ret = ReadValue(current_yaml, str);
 	if (ret != 0)
 	{
 		delete current_yaml;
 		current_yaml = nullptr;
-		return ret;
+		return -30 + ret;
 	}
-	hash->emplace(str, current_yaml);
+	hash->emplace(key, current_yaml);
 	return 0;
 }
 
@@ -296,19 +359,19 @@ int yaml::ReadList(std::vector<yaml*>* vec, const std::string& str)
 {
 	if (nullptr == vec || str.empty())
 	{
-		return -1;
+		return -40;
 	}
 	yaml* current_yaml = new yaml;
 	if (nullptr == current_yaml)
 	{
-		return -2;
+		return -50;
 	}
 	int ret = ReadValue(current_yaml, str);
 	if (ret != 0)
 	{
 		delete current_yaml;
 		current_yaml = nullptr;
-		return ret;
+		return -60 + ret;
 	}
 	vec->push_back(current_yaml);
 	return 0;
@@ -357,28 +420,187 @@ int yaml::ReadValue(yaml* current_yaml, const std::string& str)
 	}
 	else
 	{
-		return -100;
+		return -2;
 	}
 	return 0;
 }
 
-bool yaml::PushBack(yaml* parent_yaml, const std::string& key, yaml* current_yaml)
+int yaml::WriteFile(yaml* _yaml, const std::string& path)
 {
-	if (nullptr == parent_yaml || nullptr == current_yaml)
+	if (nullptr == _yaml || nullptr == _yaml->value())
 	{
-		return false;
+		return -1;
 	}
-	if (nullptr != parent_yaml->GetHash())
+	std::ofstream ofs(path);
+	if (!ofs.is_open())
 	{
-		parent_yaml->GetHash()->emplace(key, current_yaml);
-		return true;
+		return -2;
 	}
-	else if (nullptr != parent_yaml->GetVector())
+	int depth = 0;
+	int ret = yaml::WriteFileStr(ofs, _yaml, depth);
+	ofs.close();
+	return ret;
+}
+
+int yaml::WriteFileStr(std::ofstream& ofs, yaml* _yaml, int& depth)
+{
+	if (nullptr == _yaml)
 	{
-		parent_yaml->GetVector()->push_back(current_yaml);
-		return true;
+		return -1;
 	}
-	return false;
+	std::string tab;
+	for (int i = 0; i < depth; ++i)
+	{
+		tab += "\t";
+	}
+
+	static std::string next_line("\n");
+	switch (_yaml->type())
+	{
+	case YAML_TYPE_ENUM_HASH:
+	{
+		std::unordered_map<std::string, yaml*>* hash = _yaml->GetHash();
+		if (nullptr == hash)
+		{
+			break;
+		}
+
+		for (const auto& pair : *hash)
+		{
+			const std::string& key = pair.first;
+			yaml* sub_yaml = pair.second;
+			if (nullptr == sub_yaml)
+			{
+				continue;
+			}
+
+			int type = 0;
+			switch (sub_yaml->type())
+			{
+			case YAML_TYPE_ENUM_HASH: { type = WRITE_DATA_TYPE_CONTAINER; } break;
+			case YAML_TYPE_ENUM_LIST: { type = WRITE_DATA_TYPE_CONTAINER; } break;
+			case YAML_TYPE_ENUM_INTEGER: { type = WRITE_DATA_TYPE_VALUE; } break;
+			case YAML_TYPE_ENUM_LONG_LONG: { type = WRITE_DATA_TYPE_VALUE; } break;
+			case YAML_TYPE_ENUM_DOUBLE: { type = WRITE_DATA_TYPE_VALUE; } break;
+			case YAML_TYPE_ENUM_STRING: { type = WRITE_DATA_TYPE_VALUE; } break;
+			}
+			switch (type)
+			{
+			case WRITE_DATA_TYPE_CONTAINER:
+			{
+				std::string write_str = tab + key + ": " + next_line;
+				ofs.write(write_str.c_str(), write_str.size());
+				WriteFileStr(ofs, sub_yaml, ++depth);
+			}
+			break;
+			case WRITE_DATA_TYPE_VALUE:
+			{
+				std::string write_str = tab + key + ": ";
+				ofs.write(write_str.c_str(), write_str.size());
+				int temp_depth = 0;
+				WriteFileStr(ofs, sub_yaml, temp_depth);
+				ofs.write(next_line.c_str(), next_line.size());
+			}
+			break;
+			}
+		}
+	}
+	break;
+	case YAML_TYPE_ENUM_LIST:
+	{
+		std::vector<yaml*>* vec = _yaml->GetVector();
+		if (nullptr == vec)
+		{
+			break;
+		}
+		for (size_t i = 0; i < vec->size(); ++i)
+		{
+			std::string key = std::to_string(i);
+			yaml* sub_yaml = (*vec)[i];
+			if (nullptr == sub_yaml)
+			{
+				continue;
+			}
+			int type = 0;
+			switch (sub_yaml->type())
+			{
+			case YAML_TYPE_ENUM_HASH: { type = WRITE_DATA_TYPE_CONTAINER; } break;
+			case YAML_TYPE_ENUM_LIST: { type = WRITE_DATA_TYPE_CONTAINER; } break;
+			case YAML_TYPE_ENUM_INTEGER: { type = WRITE_DATA_TYPE_VALUE; } break;
+			case YAML_TYPE_ENUM_LONG_LONG: { type = WRITE_DATA_TYPE_VALUE; } break;
+			case YAML_TYPE_ENUM_DOUBLE: { type = WRITE_DATA_TYPE_VALUE; } break;
+			case YAML_TYPE_ENUM_STRING: { type = WRITE_DATA_TYPE_VALUE; } break;
+			}
+			switch (type)
+			{
+			case WRITE_DATA_TYPE_CONTAINER:
+			{
+				std::string write_str = tab + "- " + next_line;
+				ofs.write(write_str.c_str(), write_str.size());
+				WriteFileStr(ofs, sub_yaml, ++depth);
+			}
+			break;
+			case WRITE_DATA_TYPE_VALUE:
+			{
+				std::string write_str = tab + "- ";
+				ofs.write(write_str.c_str(), write_str.size());
+				int temp_depth = 0;
+				WriteFileStr(ofs, sub_yaml, temp_depth);
+				ofs.write(next_line.c_str(), next_line.size());
+			}
+			break;
+			}
+		}
+	}
+	break;
+	case YAML_TYPE_ENUM_INTEGER:
+	{
+		int* value_ptr = _yaml->GetInt();
+		if (nullptr == value_ptr)
+		{
+			break;
+		}
+		std::string write_str = std::to_string(*value_ptr);
+		ofs.write(write_str.c_str(), write_str.size());
+	}
+	break;
+	case YAML_TYPE_ENUM_LONG_LONG:
+	{
+		long long* value_ptr = _yaml->GetLongLong();
+		if (nullptr == value_ptr)
+		{
+			break;
+		}
+		std::string write_str = std::to_string(*value_ptr);
+		ofs.write(write_str.c_str(), write_str.size());
+	}
+	break;
+	case YAML_TYPE_ENUM_DOUBLE:
+	{
+		double* value_ptr = _yaml->GetDouble();
+		if (nullptr == value_ptr)
+		{
+			break;
+		}
+		std::string write_str = std::to_string(*value_ptr);
+		ofs.write(write_str.c_str(), write_str.size());
+	}
+	break;
+	case YAML_TYPE_ENUM_STRING:
+	{
+		std::string* value_ptr = _yaml->GetStr();
+		if (nullptr == value_ptr)
+		{
+			break;
+		}
+		std::string write_str = *value_ptr;
+		ofs.write(write_str.c_str(), write_str.size());
+	}
+	break;
+	}
+
+	--depth;
+	return 0;
 }
 
 std::vector<yaml*>* yaml::GetVector()
@@ -423,12 +645,45 @@ double* yaml::GetDouble()
 	}
 	return m_value._double;
 }
-yaml* yaml::operator[](const std::string& key)
+std::string* yaml::GetStr()
 {
-	if (key.empty())
+	if (YAML_TYPE_ENUM_STRING != m_type)
 	{
 		return nullptr;
 	}
+	return m_value._string;
+}
+
+size_t yaml::Size()
+{
+	switch (m_type)
+	{
+	case YAML_TYPE_ENUM_HASH:
+	{
+		auto* container = this->GetHash();
+		if (nullptr == container)
+		{
+			break;
+		}
+		return container->size();
+	}
+	break;
+	case YAML_TYPE_ENUM_LIST:
+	{
+		auto* container = this->GetVector();
+		if (nullptr == container)
+		{
+			break;
+		}
+		return container->size();
+	}
+	break;
+	}
+	return 0;
+}
+
+yaml* yaml::at(const std::string& key)
+{
 	std::size_t pos = key.find_first_of('/');
 	std::string one_key;
 	std::string other_key;
@@ -467,7 +722,7 @@ yaml* yaml::operator[](const std::string& key)
 		{
 			return _yaml;
 		}
-		return (*_yaml)[other_key];
+		return _yaml->at(other_key);
 	}
 	break;
 	case YAML_TYPE_ENUM_LIST:
@@ -497,19 +752,11 @@ yaml* yaml::operator[](const std::string& key)
 		{
 			return _yaml;
 		}
-		return (*_yaml)[other_key];
+		return _yaml->at(other_key);
 	}
 	break;
 	}
 	return nullptr;
-}
-std::string* yaml::GetStr()
-{
-	if (YAML_TYPE_ENUM_STRING != m_type)
-	{
-		return nullptr;
-	}
-	return m_value._string;
 }
 
 size_t yaml::GetTabCount(const std::string& str)
@@ -527,5 +774,18 @@ size_t yaml::GetTabCount(const std::string& str)
 		++count;
 	}
 	return count;
+}
+
+void yaml::CheckResize(size_t tab_count)
+{
+	if (tab_count > m_last_line_tab_count)
+	{
+		return;
+	}
+	const size_t cur_size = tab_count + 1;
+	if (m_yaml_vec.size() > cur_size)
+	{
+		m_yaml_vec.resize(cur_size);
+	}
 }
 
--- code/3rdpart/yaml/yaml.h
+++ code/3rdpart/yaml/yaml.h
@@ -15,6 +15,12 @@ enum YAML_TYPE_ENUM
 	YAML_TYPE_ENUM_STRING,
 };
 
+enum WRITE_DATA_TYPE
+{
+	WRITE_DATA_TYPE_CONTAINER = 1,
+	WRITE_DATA_TYPE_VALUE = 2,
+};
+
 class yaml;
 union yaml_value
 {
@@ -43,15 +49,15 @@ public:
 
 	static int ReadFileList();
 	static int ReadFile(const std::string& str);
-	static int WriteFile(yaml* _yaml, const std::string& path);
-	static int WriteFileStr(yaml* _yaml, int& depth);
 	static int ReadFileName(const std::string& str);
 	static int ReadLine(int line, const std::string& str);
+	static int PushValue(yaml* parent_yaml, const std::string& key, const std::string& str);
+	static bool PushBack(yaml* parent_yaml, const std::string& key, yaml* current_yaml);
 	static int ReadHash(std::unordered_map<std::string, yaml*>* hash, const std::string& key, const std::string& str);
 	static int ReadList(std::vector<yaml*>* vec, const std::string& str);
 	static int ReadValue(yaml* current_yaml, const std::string& str);
-	static bool PushBack(yaml* parent_yaml, const std::string& key, yaml* current_yaml);
-	yaml* operator[](const std::string& key);
+	static int WriteFile(yaml* _yaml, const std::string& path);
+	static int WriteFileStr(std::ofstream& ofs, yaml* _yaml, int& depth);
 
 	std::vector<yaml*>* GetVector();
 	std::unordered_map<std::string, yaml*>* GetHash();
@@ -59,8 +65,10 @@ public:
 	long long* GetLongLong();
 	double* GetDouble();
 	std::string* GetStr();
+	size_t Size();
+	yaml* at(const std::string& key);
 
-	int type() { return m_type; }
+	int type() const { return m_type; }
 	void mutate_type(int type) { m_type = type; }
 
 	void* value() { return m_value._void; }
@@ -68,11 +76,12 @@ public:
 
 protected:
 	static size_t GetTabCount(const std::string& str);
+	static void CheckResize(size_t tab_count);
 
 
 private:
 	static std::vector<yaml*> m_yaml_vec;
-	static std::vector<std::string> m_key_vec;
+	static size_t m_last_line_tab_count;
 	int m_type;
 	yaml_value m_value;
 };
